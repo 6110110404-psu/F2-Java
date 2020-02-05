656d5779f79bc6576dad5a8668fcb6adbad6488d
6110110404-psu <60681482+6110110404-psu@users.noreply.github.com>
2020-02-05 13:30:37 +0700

    Add files via upload

---
+++ PokemonGame.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import Pokemon.java;
+public class PokemonGame{
+public static void main (String args[])
+    {
+        Pokemon  lugia = new Pokemon(lugiaHP,lugiaATK);
+        system.out.println(lugiaHP);
+        system.out.println(lugiaATK);
+    }
+} 
